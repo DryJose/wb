46f0c0984b44de81ff455545ac35293d3f2a34e4
Arthur M <98966593+DryJose@users.noreply.github.com>
2024-01-07 00:39:15 +0300

    fix4

--- data.js
+++ data.js
@@ -125,22 +125,22 @@ export const products = [
   export const paymentInfo = [
     {
       name: 'mir',
-      logo: '.favicon/mir.svg',
+      logo: '/favicon/mir.svg',
       number: '1234 56•• •••• 1234'
     },
     {
       name: 'visa',
-      logo: '.favicon/visa.svg',
+      logo: '/favicon/visa.svg',
       number: '1234 56•• •••• 1234'
     },
     {
       name: 'mastercard',
-      logo: '.favicon/mastercard.svg',
+      logo: '/favicon/mastercard.svg',
       number: '1234 56•• •••• 1234'
     },
     {
       name: 'maestro',
-      logo: '.favicon/maestro.svg',
+      logo: '/favicon/maestro.svg',
       number: '1234 56•• •••• 1234'
     }
   ]
